2073182c6c5c98aecaee693c1671308c3e2ea9fe
Filimon Kiros <filimona.kiros@gmail.com>
2022-06-26 13:23:33 -0400

    related items dir&file setup

---
+++ client/src/components/relatedItems/Outfit.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Outfit extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>Your Outfit</div>;
+  }
+}
+
+export default Outfit;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App_RelatedAndOutfit from "./components/relatedItems/index.jsx";
 
 const App = () => {
   return (
     <div>
       <h1>Testing</h1>
+      <App_RelatedAndOutfit />
     </div>
   );
-}
+};
 
 export default App;
 
-
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
-
+ReactDOM.createRoot(document.getElementById("app")).render(<App />);
