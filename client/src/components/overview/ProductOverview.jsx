5cff256aa83ed3af65ca38bb7b3860cc6c8df648
Jason <jason.novacovici@gmail.com>
2022-07-07 15:39:31 -0400

    Finish API implementation for styleselector

--- client/src/components/overview/ProductOverview.jsx
+++ client/src/components/overview/ProductOverview.jsx
@@ -10,28 +10,60 @@ class ProductOverview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chosenProduct: {},
-      style: [],
-      selectedStyle: '',
+      styles: [],
+      selectedStyle: undefined,
       selectedPhoto: '',
     };
+    this.selectedStyle = this.selectedStyle.bind(this);
+    this.changeStyle = this.changeStyle.bind(this);
+    this.testFunc = this.testFunc.bind(this);
   }
   componentDidMount() {
+    this.selectedStyle();
+  }
 
+  selectedStyle() {
+    var tempStyle = this.props.defaultStyle;
+    this.setState({
+      selectedStyle: tempStyle,
+      styles: this.props.styleList
+    }, this.testFunc());
+    console.log('temp style', tempStyle);
+  }
+
+  testFunc() {
+    console.log('new state ', this);
+  }
+
+  changeStyle(id) {
+    this.setState({ selectedStyle: this.props.styleList.find((element) => element.style_id === id)});
   }
 
   render() {
     console.log(this.props.productInfo);
     if (this.props.productInfo && this.props.defaultStyle) {
+      console.log('this is the current style ', this.state.selectedStyle);
       return (
         <div>
           <h1>Product Overview!</h1>
           {/* <PrimaryImage /> */}
           {/* <Checkout /> */}
           {/* <ProductDescription slogan={this.props.productInfo.slogan} description={this.props.productInfo.description} features={this.props.productInfo.features}/> */}
+
           {/* <ProductInformation category={this.props.productInfo.category} name={this.props.productInfo.name}
             price={this.props.defaultStyle.original_price} salePrice={this.props.defaultStyle.sale_price}/> */}
-          <SelectStyle />
+          {this.state.selectedStyle ?
+            <div>
+              <ProductInformation category={this.props.productInfo.category} name={this.props.productInfo.name}
+                price={this.state.selectedStyle.original_price} salePrice={this.state.selectedStyle.sale_price}/>
+              <SelectStyle styles={this.props.styleList} changeStyle={this.changeStyle} selectedStyle={this.state.selectedStyle}/>
+            </div> :
+            <div>
+              <ProductInformation category={this.props.productInfo.category} name={this.props.productInfo.name}
+                price={this.props.defaultStyle.original_price} salePrice={this.props.defaultStyle.sale_price}/>
+              <SelectStyle styles={this.props.styleList} changeStyle={this.changeStyle} selectedStyle={this.props.defaultStyle}/>
+            </div>
+          }
         </div>
       );
     }
