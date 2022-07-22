a5460e7c4f1516ad31c11ab1338a2a1b54b7c7fe
Ai Lam <ai_lam@outlook.com>
2022-07-22 15:47:54 -0700

    Complete reviewBody characters count

--- client/src/components/ratingsAndReviews/Reviews/ReviewModal.jsx
+++ client/src/components/ratingsAndReviews/Reviews/ReviewModal.jsx
@@ -6,9 +6,11 @@ class ReviewModal extends React.Component {
     super(props);
     this.state = {
       clickedStar: null,
-      message: null
+      starMessage: null,
+      reviewBodyMessage: 'Minimum required characters left: 50'
     };
     this.clickFillStarFunc = this.clickFillStarFunc.bind(this);
+    this.charactersLeftFunc = this.charactersLeftFunc.bind(this);
     this.ratingMessage = {
       1: 'Poor',
       2: 'Fair',
@@ -19,8 +21,24 @@ class ReviewModal extends React.Component {
   }
 
   clickFillStarFunc(star) {
-    const message = this.ratingMessage[star];
-    this.setState({clickedStar: star, message});
+    const starMessage = this.ratingMessage[star];
+    this.setState({clickedStar: star, starMessage});
+  }
+
+  charactersLeftFunc () {
+    const textarea = document.getElementById('reviewBody');
+    console.dir(textarea);
+    const minLength = 50;
+    const maxLength = 1000;
+    const currentTextLength = textarea.value.length;
+
+    if (currentTextLength < minLength) {
+      const remainingCharacters = 50 - currentTextLength;
+      this.setState({reviewBodyMessage: `Minimum required characters left: ${remainingCharacters}`});
+    } else if (currentTextLength >= minLength) {
+      this.setState({reviewBodyMessage: `Minimum reached. Characters count: ${currentTextLength}`});
+    }
+
   }
 
   render() {
@@ -39,8 +57,6 @@ class ReviewModal extends React.Component {
       margin: 'auto 10px'
     };
 
-
-
     return (
       <div className="reviewModal ">
         <div className="reviewModal-content reviewScrollable">
@@ -48,80 +64,95 @@ class ReviewModal extends React.Component {
           <div>
             <h2>Write Your Review</h2>
             <h3>About the {this.props.productInfo.name}</h3>
-            <h3>Overall Rating<sup>*</sup></h3>
-            <div style={sameLineStyle}>
-              <StarReviewIcon starNumber={1} clickFillStarFunc={this.clickFillStarFunc} filled={1 <= this.state.clickedStar} />
-              <StarReviewIcon starNumber={2} clickFillStarFunc={this.clickFillStarFunc} filled={2 <= this.state.clickedStar} />
-              <StarReviewIcon starNumber={3} clickFillStarFunc={this.clickFillStarFunc} filled={3 <= this.state.clickedStar} />
-              <StarReviewIcon starNumber={4} clickFillStarFunc={this.clickFillStarFunc} filled={4 <= this.state.clickedStar} />
-              <StarReviewIcon starNumber={5} clickFillStarFunc={this.clickFillStarFunc} filled={5 <= this.state.clickedStar} />
-              <div style={messageStyle}>{this.state.message}</div>
-            </div>
-            <h3>Do you recommend this product?<sup>*</sup></h3>
-            <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Yes</label>
-            <input type="radio" id="" name="one" value="no"></input><label htmlFor="">No</label>
-            <h3>Characteristics</h3>
+
             <div>
-              <div>
-                <b>Size</b>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">A size too small</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor=""><sup>1</sup>/<sub>2</sub> a size too small</label>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor=""><sup>1</sup>/<sub>2</sub> a size too big</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">A size too wide</label>
-              </div>
-
-              <div>
-                <b>Width</b>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Too narrow</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Slightly narrow</label>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Slightly wide</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Too wide</label>
-              </div>
-
-              <div>
-                <b>Comfort</b>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Uncomfortable</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Slightly uncomfortable</label>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Ok</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Comfortable</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Perfect</label>
-              </div>
-
-              <div>
-                <b>Quality</b>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Poor</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Below average</label>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">What I expected</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Pretty great</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Perfect</label>
-              </div>
-
-              <div>
-                <b>Length</b>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Runs short</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly short</label>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly long</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs long</label>
-              </div>
-
-              <div>
-                <b>Fit</b>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Runs tight</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly tight</label>
-                <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly long</label>
-                <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs long</label>
+              <h3>Overall Rating<sup>*</sup></h3>
+              <div style={sameLineStyle}>
+                <StarReviewIcon starNumber={1} clickFillStarFunc={this.clickFillStarFunc} filled={1 <= this.state.clickedStar} />
+                <StarReviewIcon starNumber={2} clickFillStarFunc={this.clickFillStarFunc} filled={2 <= this.state.clickedStar} />
+                <StarReviewIcon starNumber={3} clickFillStarFunc={this.clickFillStarFunc} filled={3 <= this.state.clickedStar} />
+                <StarReviewIcon starNumber={4} clickFillStarFunc={this.clickFillStarFunc} filled={4 <= this.state.clickedStar} />
+                <StarReviewIcon starNumber={5} clickFillStarFunc={this.clickFillStarFunc} filled={5 <= this.state.clickedStar} />
+                <div style={messageStyle}>{this.state.starMessage}</div>
               </div>
             </div>
 
-            <h3>Review summary</h3>
-            <textarea type="input" className="reviewSummary" maxLength="60" placeholder="Example: Best purchase ever!"></textarea>
+            <div>
+              <h3>Do you recommend this product?<sup>*</sup></h3>
+              <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Yes</label>
+              <input type="radio" id="" name="one" value="no"></input><label htmlFor="">No</label>
+            </div>
 
-            <h3>Review body<sup>*</sup></h3>
-            <textarea type="input" className="reviewBody" minLength="50" maxLength="1000" placeholder="Why did you like the product or not?"></textarea>
+            <div>
+              <h3>Characteristics</h3>
+              <div>
+                <div>
+                  <b>Size</b>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">A size too small</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor=""><sup>1</sup>/<sub>2</sub> a size too small</label>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor=""><sup>1</sup>/<sub>2</sub> a size too big</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">A size too wide</label>
+                </div>
+
+                <div>
+                  <b>Width</b>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Too narrow</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Slightly narrow</label>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Slightly wide</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Too wide</label>
+                </div>
+
+                <div>
+                  <b>Comfort</b>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Uncomfortable</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Slightly uncomfortable</label>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Ok</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Comfortable</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Perfect</label>
+                </div>
+
+                <div>
+                  <b>Quality</b>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Poor</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Below average</label>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">What I expected</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Pretty great</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Perfect</label>
+                </div>
+
+                <div>
+                  <b>Length</b>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Runs short</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly short</label>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly long</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs long</label>
+                </div>
+
+                <div>
+                  <b>Fit</b>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Runs tight</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly tight</label>
+                  <input type="radio" id="" name="one" value="yes"></input><label htmlFor="">Perfect</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs slightly long</label>
+                  <input type="radio" id="" name="one" value="no"></input><label htmlFor="">Runs long</label>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <h3>Review summary</h3>
+              <textarea type="input" className="reviewSummary" maxLength="60" placeholder="Example: Best purchase ever!"></textarea>
+            </div>
+
+            <div>
+              <h3>Review body<sup>*</sup></h3>
+              <textarea type="input" id="reviewBody" className="reviewBody" minLength="50" maxLength="1000" placeholder="Why did you like the product or not?" onChange={() => { this.charactersLeftFunc(); }}></textarea>
+              <div>{this.state.reviewBodyMessage}</div>
+            </div>
+
           </div>
         </div>
       </div>
