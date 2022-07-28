7862fcaf7b64ae19652a6a2c2ef22048c5aa15ca
Jason <jason.novacovici@gmail.com>
2022-07-28 11:39:53 -0400

    Fixed zoom functionality

--- client/src/components/overview/imageGallery/RenderZoom.jsx
+++ client/src/components/overview/imageGallery/RenderZoom.jsx
@@ -19,19 +19,6 @@ export default function RenderZoom({ picture, container, zoomOut }) {
     return (((val - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin);
   };
 
-  useEffect(() => {
-    const zoom = document.getElementById('zoomEle');
-
-    const changeSize = () => {
-      setContainerState(document.getElementById('bigImageContainer'));
-    };
-
-    containerState.addEventListener('resize', changeSize);
-    return () => {
-      containerState.removeEventListener('resize', changeSize);
-    };
-  }, []);
-
   const checkMouse = (e) => {
     const border = zoomEle.current.getBoundingClientRect();
     var xPosition = e.clientX - border.left;
@@ -57,72 +44,9 @@ export default function RenderZoom({ picture, container, zoomOut }) {
       id='zoomEle'
       ref={zoomEle}
       photoUrl={pictureState.src}
+      onMouseOver={(e) => checkMouse(e)}
       onMouseMove={(e) => checkMouse(e)}
       onClick={() => zoomOut()}
     />
   );
 }
-
-
-// import React from 'react';
-// import styled, { css } from 'styled-components';
-
-// const Div = styled.div`
-//   position: absolute;
-//   width: 100%;
-//   height: 100%;
-//   transform-origin: center center;
-//   z-index: 15;
-//   background-repeat: no-repeat;
-//   background-image: ${({ photoUrl }) => css`url(${photoUrl});`}
-// `;
-
-// class RenderZoom extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       zoomEle: null
-//     };
-//     this.checkMouse = this.checkMouse.bind(this);
-//     this.calculatePosition = this.calculatePosition.bind(this);
-//   }
-
-//   calculatePosition(val, inMin, inMax, outMin, outMax) {
-//     return (((val - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin);
-//   }
-
-//   checkMouse(e) {
-//     var zoomStateHolder = this.state.zoomEle;
-//     const border = this.state.zoomEle.current.getBoundingClientRect();
-//     var xPosition = e.clientX - border.left;
-//     var yPosition = e.clientY - border.top;
-
-//     const zoomWidth = this.state.zoomEle.current.clientWidth;
-//     const backgroundSizeX = this.calculatePosition(xPosition, 0, zoomWidth, 0, zoomWidth * 2.5 - zoomWidth);
-
-//     const zoomHeight = this.state.zoomEle.current.clientHeight;
-//     const imageRatio = this.props.picture.width / this.props.picture.height;
-//     const imageHeight = (this.props.picture.height * zoomWidth) / this.props.picture.width;
-//     imageHeight *= 2.5;
-
-//     const backgroundSizeY = this.calculatePosition(yPosition, 0, zoomHeight, 0, imageHeight - zoomHeight);
-
-//     zoomStateHolder.current.style.backgroundSize = `${zoomWidth * 2.5}px ${imageHeight}px`;
-//     zoomStateHolder.current.style.backgroundPosition = `-${backgroundSizeX}px -${backgroundSizeY}px`;
-//     zoomStateHolder.current.style.cursor = 'zoom-out';
-//   }
-
-//   render() {
-//     return (
-//       <Div
-//         id='zoomEle'
-//         ref={this.state.zoomEle}
-//         photoUrl={this.props.picture.src}
-//         onMouseMove={(e) => this.checkMouse(e)}
-//         onClick={() => this.props.zoomOut()}
-//       />
-//     );
-//   }
-// }
-
-// export default RenderZoom;
